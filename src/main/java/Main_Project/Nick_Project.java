c363c3fcec3491a7a33dd8bcd376d2574f6acf24
nick.schroeder <nick.schroeder@della201-03.ad.maxe-del.de>
2023-09-11 10:56:44 +0200

    First test

---
+++ src/main/java/Main_Project/Nick_Project.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package Main_Project;
+
+/**
+ *
+ * @author frankjuchim
+ */
+public class Nick_Project {
+
+    public static void main(String args[]) {
+       GUI main = new GUI();
+       main.setVisible(true);
+    }
+
+}
